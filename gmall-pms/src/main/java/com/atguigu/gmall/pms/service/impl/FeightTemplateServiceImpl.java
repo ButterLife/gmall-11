7bfc30f0a7d6fe06b3b8672985bebc52d622edef
Hopeful <807866012@qq.com>
2020-04-25 22:27:25 +0800

    逆向工程生产类

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/FeightTemplateServiceImpl.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.pms.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.pms.entity.FeightTemplate;
+import com.atguigu.gmall.pms.mapper.FeightTemplateMapper;
+import com.atguigu.gmall.pms.service.FeightTemplateService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * <p>
+ * 运费模版 服务实现类
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-04-24
+ */
+@Component
+@Service
+public class FeightTemplateServiceImpl extends ServiceImpl<FeightTemplateMapper, FeightTemplate> implements FeightTemplateService {
+
+}
